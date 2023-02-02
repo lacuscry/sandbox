0cc31fa10012c6ae9a8c35d97369f6c71624ad02
Tim <timuravezov123@mail.ru>
2023-02-03 03:08:59 +0500

    48

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,6 +1,6 @@
-// Setup
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-
-// Only change code below this line
-myArray.unshift(['Paul', 35]);
+const myList = [];
+myList.push(['one', 2, 3]);
+myList.push(['one', 2, 3]);
+myList.push(['one', 2, 3]);
+myList.push(['one', 2, 3]);
+myList.push(['one', 2, 3]);
