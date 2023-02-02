962da5fafb6231383379b2c01f84f2325921d05b
Tim <timuravezov123@mail.ru>
2023-02-02 05:27:27 +0500

    05

--- codewars.js
+++ codewars.js
@@ -1,1 +1,9 @@
-const quarterOf = month => Math.ceil(month / 12 * 4);
+function largest(n, array) {
+    const filterArr = array.sort((a, b) => a - b);
+  
+    if (n) {
+      return filterArr.slice(-n);
+    } else {
+      return filterArr.slice(filterArr.length);
+    }
+}
