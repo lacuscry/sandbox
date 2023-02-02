be4932e205bc48bee93daa49d417531adf16e537
Tim <timuravezov123@mail.ru>
2023-02-03 03:01:13 +0500

    42

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,2 +1,7 @@
-const myArray = [50, 60, 70];
-const myData = myArray[0];
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+myArray[1] = 64;
+myArray[2] = 99;
