673047dd5d955cad4aa9c24438127fb72c311b2c
Tim <timuravezov123@mail.ru>
2023-02-04 06:56:24 +0500

    74

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,11 +1,17 @@
-function orderMyLogic(val) {
-    if (val < 5) {
-      return "Less than 5";
-    } else if (val < 10) {
-      return "Less than 10";
+function testSize(num) {
+    // Only change code below this line
+    if (num > 19) {
+      return 'Huge';
+    } else if (num > 14) {
+      return 'Large';
+    } else if (num > 9) {
+      return 'Medium';
+    } else if (num > 4) {
+      return 'Small';
     } else {
-      return "Greater than or equal to 10";
+      return 'Tiny';
     }
+    // Only change code above this line
   }
   
-  orderMyLogic(7);
+  testSize(7);
