ae52e71a5556816ed89cad8e229789d96ccab1c2
Tim <timuravezov123@mail.ru>
2023-02-04 09:10:57 +0500

    82

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,16 +1,12 @@
-// Setup
-function abTest(a, b) {
-    // Only change code below this line
-  
-    if (b === 0 && 0 === a) {
-      return 0;
-    } else if (a + b < 1) {
-      return undefined;
-    }
-  
-    // Only change code above this line
-  
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-  }
-  
-  abTest(2,2);
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  card < 7 && count++;
+  (10 === card || typeof card === 'string') && count--;
+
+  return count > 0 ? `${count} Bet` : `${count} Hold`;
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
