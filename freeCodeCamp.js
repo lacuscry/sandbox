3e3cf7d5396970b5e95461719e3edccce8609eca
Tim <timuravezov123@mail.ru>
2023-02-04 06:32:58 +0500

    58

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,16 +1,7 @@
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
     // Only change code below this line
-    arr.push(item);
-    const removed = arr[0];
-    arr.shift(arr[0]);
-    return removed;
+  
+    return true; // Change this line
+  
     // Only change code above this line
   }
-  
-  // Setup
-  let testArr = [1, 2, 3, 4, 5];
-  
-  // Display code
-  console.log("Before: " + JSON.stringify(testArr));
-  console.log(nextInLine(testArr, 6));
-  console.log("After: " + JSON.stringify(testArr));
