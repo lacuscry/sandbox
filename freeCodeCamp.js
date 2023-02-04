0256aa80e65514f349226dce67a72cd78dd19682
Tim <timuravezov123@mail.ru>
2023-02-04 07:25:28 +0500

    77

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,29 +1,23 @@
-function caseInSwitch(val) {
+function switchOfStuff(val) {
     let answer = "";
     // Only change code below this line
     switch(val) {
-      case 1:
-        return 'alpha';
-        
+      case 'a':
+        return 'apple';
         break;
+      case 'b':
+        return 'bird';
+        break;
+      case 'c':
+        return 'cat';
+        break;
+      default: 
+        return 'stuff';
+  }
   
-      case 2:
-          return 'beta';
-        
-          break;
   
-      case 3:
-          return 'gamma';
-        
-          break;
-  
-      case 4:
-          return 'delta';
-        
-          break;
-    };
     // Only change code above this line
     return answer;
   }
   
-  caseInSwitch(1);
+  switchOfStuff(1);
