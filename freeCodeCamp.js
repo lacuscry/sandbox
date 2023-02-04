ef1df9b3aab908fadb5ff35cca20475046f5305a
Tim <timuravezov123@mail.ru>
2023-02-04 11:35:57 +0500

    105

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,40 +1,8 @@
-// Setup
-const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
-  
-  function lookUpProfile(name, prop) {
+function randomFraction() {
+
     // Only change code below this line
-    for (let i = 0; i < contacts.length; i++) {
-      if (contacts[i].firstName === name) {
-        return contacts[i].hasOwnProperty(prop) ? contacts[i][prop] : 'No such property';
-      } else if (i === contacts.length - 1) {
-        return `No such contact`;
-      }
-    }
-  }
   
-  lookUpProfile("Akira", "likes");
+    return Math.random();
+  
+    // Only change code above this line
+  }
