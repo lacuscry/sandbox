17efc176d86f8774cf49c0dcf326a2f2d5e78c15
Tim <timuravezov123@mail.ru>
2023-02-04 09:14:29 +0500

    83

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,12 +1,8 @@
-let count = 0;
-
-function cc(card) {
-  // Only change code below this line
-  card < 7 && count++;
-  (10 === card || typeof card === 'string') && count--;
-
-  return count > 0 ? `${count} Bet` : `${count} Hold`;
-  // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+const myDog = {
+    // Only change code below this line
+    name: 'Snoop',
+    legs: 4,
+    tails: 1,
+    friends: ['bones', 'bed']
+    // Only change code above this line
+  };
