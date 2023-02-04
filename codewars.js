a6657cdf15c26d4b585d995a71262d12cd3e5141
Tim <timuravezov123@mail.ru>
2023-02-04 15:27:04 +0500

    23

--- codewars.js
+++ codewars.js
@@ -1,1 +1,15 @@
-const getAge = age => parseInt(age);
+function getGrade (...grades) {
+  const score = Math.floor(grades.reduce((acc, num, arr) => acc + num) / grades.length);
+
+  if (90 <= score) {
+    return 'A';
+  } else if (80 <= score) {
+      return 'B';
+  } else if (70 <= score) {
+      return 'C';
+  } else if (60 <= score) {
+      return 'D';
+  } else {
+    return 'F';
+  }
+}
