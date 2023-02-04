5adca126e80cdcd0dfc347285df2eed3a5ea3919
Tim <timuravezov123@mail.ru>
2023-02-04 10:28:28 +0500

    100

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,7 +1,9 @@
 // Setup
-const myArray = [];
+const myArr = [2, 3, 4, 5, 6];
 
 // Only change code below this line
-for (let i = 9; i >= 1; i -= 2) {
-    myArray.push(i);
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
 };
