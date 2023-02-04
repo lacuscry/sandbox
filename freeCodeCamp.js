d4e744d56ccce60df294887ab27352262175ec32
Tim <timuravezov123@mail.ru>
2023-02-04 06:32:05 +0500

    57

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,7 +1,16 @@
-// Setup
-let processed = processArg(7);
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-// Only change code below this line
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    const removed = arr[0];
+    arr.shift(arr[0]);
+    return removed;
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
