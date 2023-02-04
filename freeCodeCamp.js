40395149bd0d058a209bb3528bb7b7b51d5c8dfd
Tim <timuravezov123@mail.ru>
2023-02-04 11:35:23 +0500

    104

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,9 +1,40 @@
-function sum(arr, n) {
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
     // Only change code below this line
-      if (n <= 0) {
-        return n;
-      } else {
-        return sum(arr, n - 1) + arr[n - 1];
+    for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        return contacts[i].hasOwnProperty(prop) ? contacts[i][prop] : 'No such property';
+      } else if (i === contacts.length - 1) {
+        return `No such contact`;
       }
-    // Only change code above this line
+    }
   }
+  
+  lookUpProfile("Akira", "likes");
