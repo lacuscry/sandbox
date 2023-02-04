f745141fdc42be3116b3202f94fec7de1eee4bfd
Tim <timuravezov123@mail.ru>
2023-02-04 06:34:42 +0500

    59

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,7 +1,5 @@
-function welcomeToBooleans() {
+function trueOrFalse(wasThatTrue) {
     // Only change code below this line
-  
-    return true; // Change this line
-  
-    // Only change code above this line
-  }
+    return wasThatTrue ? 'Yes, that was true' : 'No, that was false';
+    // Only change code above this line  
+}
