9c53d144c9d808582013a2f55ce067082b63500d
Tim <timuravezov123@mail.ru>
2023-02-04 06:20:18 +0500

    56

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,17 +1,7 @@
 // Setup
-let sum = 0;
+let processed = processArg(7);
 
-function addThree() {
-  sum = sum + 3;
+function processArg(num) {
+  return (num + 3) / 5;
 }
-
 // Only change code below this line
-function addFive() {
-  sum += 5;
-  return undefined;
-}
-
-// Only change code above this line
-
-addThree();
-addFive();
