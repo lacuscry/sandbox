258b864eaa75448dda134251cf87f9a1ab8eafdf
Tim <timuravezov123@mail.ru>
2023-02-04 09:43:31 +0500

    93

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,20 +1,13 @@
-const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T"
-      ]
-    },
-    {
-      "artist": "Billy",
-      "title": "Piano",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T"
-      ]
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
     }
-  ];
+  };
+  
+  const gloveBoxContents = myStorage.car.inside['glove box'];
