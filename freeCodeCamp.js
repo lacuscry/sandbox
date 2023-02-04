de32a27286e518dd02ea273bcf2fcabdb838c6c1
Tim <timuravezov123@mail.ru>
2023-02-04 07:18:04 +0500

    75

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,17 +1,22 @@
-function testSize(num) {
-    // Only change code below this line
-    if (num > 19) {
-      return 'Huge';
-    } else if (num > 14) {
-      return 'Large';
-    } else if (num > 9) {
-      return 'Medium';
-    } else if (num > 4) {
-      return 'Small';
-    } else {
-      return 'Tiny';
-    }
-    // Only change code above this line
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+    if (strokes === 1) {
+    return 'Hole-in-one!';
+  } else if (strokes <= par - 2) {
+    return 'Eagle';
+  } else if (strokes === par - 1) {
+    return 'Birdie';
+  } else if (strokes === par) {
+    return 'Par';
+  } else if (strokes === par + 1) {
+    return 'Bogey';
+  } else if (strokes === par + 2) {
+    return 'Double Bogey';
+  } else if(strokes >= par + 3) {
+    return 'Go Home!';
   }
-  
-  testSize(7);
+}
+
+golfScore(5, 4);
