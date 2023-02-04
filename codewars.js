845732dea9de088c38a499a79e180035b72d1dcf
Tim <timuravezov123@mail.ru>
2023-02-04 15:48:27 +0500

    24

--- codewars.js
+++ codewars.js
@@ -1,15 +1,7 @@
-function getGrade (...grades) {
-  const score = Math.floor(grades.reduce((acc, num, arr) => acc + num) / grades.length);
-
-  if (90 <= score) {
-    return 'A';
-  } else if (80 <= score) {
-      return 'B';
-  } else if (70 <= score) {
-      return 'C';
-  } else if (60 <= score) {
-      return 'D';
-  } else {
-    return 'F';
-  }
-}
+function updateLight (current) {
+  switch (current) {
+      case 'green': return 'yellow';
+      case 'yellow': return 'red';
+      case 'red': return 'green';
+  };
+};
