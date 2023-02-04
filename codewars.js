d1db5f500a001f76e63291b2ff732304f9d7df00
Tim <timuravezov123@mail.ru>
2023-02-04 14:29:17 +0500

    20

--- codewars.js
+++ codewars.js
@@ -1,9 +1,1 @@
-function repeatStr (n, s) {
-  let string = s;
-  
-  for (let i = 1; i < n; i++) {
-    string += s;
-  }
-  
-  return string;
-}
+const isIsogram = str => ![...str.toLowerCase()].filter((l, i, a) => i !== a.indexOf(l)).length;
