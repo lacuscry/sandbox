afe487b8c3d60105467500712ae79664e1e6fb07
Tim <timuravezov123@mail.ru>
2023-02-04 11:54:45 +0500

    112

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,5 +1,11 @@
-function checkSign(num) {
-    return num > 0 ? 'positive' : num === 0 ? 'zero' : 'negative';
+// Only change code below this line
+function countdown(n){
+    if (n < 1) {
+      return [];
+    } else {
+      const arr = countdown(n - 1);
+      arr.unshift(n);
+      return arr;
+    }
   }
-  
-  checkSign(10);
+  // Only change code above this line
