69e16619b5f36ec4c45539f0f41b16322a1ac8f7
Tim <timuravezov123@mail.ru>
2023-02-04 06:15:45 +0500

    54

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,10 +1,11 @@
-function myLocalScope() {
-    // Only change code below this line
-    const myVar = 1;
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();
-  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
-  console.log('outside myLocalScope', myVar);
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  return 'sweater';
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
