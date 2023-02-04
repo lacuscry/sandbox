07bbe097fd906c16e13f667e6e96cc06f29ec1ba
Tim <timuravezov123@mail.ru>
2023-02-04 20:05:46 +0500

    33

--- codewars.js
+++ codewars.js
@@ -1,1 +1,29 @@
-const stray = nums => +nums.filter((n, i, a) => a.indexOf(n) === a.lastIndexOf(n));
+function humanYearsCatYearsDogYears(years) {
+  const result = [0, 0, 0];
+  
+  const yearsInfo = [
+    {
+      1: 1,
+      2: 1,
+      3: 1
+    },
+    {
+      1: 15,
+      2: 9,
+      3: 4
+    },
+    {
+      1: 15,
+      2: 9,
+      3: 5
+    }
+  ]
+  
+  yearsInfo.forEach((el, i) => {
+    for (let y = 1; y <= years; y++) {
+         result[i] += y < 4 ? el[y] : el[3];
+    }
+  }); 
+
+  return result;
+};
