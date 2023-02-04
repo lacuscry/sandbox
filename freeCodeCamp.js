4dcadfb0e5f07ac3ab546b6bbaaae0f69995d335
Tim <timuravezov123@mail.ru>
2023-02-04 07:27:13 +0500

    78

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,23 +1,29 @@
-function switchOfStuff(val) {
+function sequentialSizes(val) {
     let answer = "";
     // Only change code below this line
     switch(val) {
-      case 'a':
-        return 'apple';
+      case 1:
+      case 2:
+      case 3:
+        return 'Low';
         break;
-      case 'b':
-        return 'bird';
+  
+      case 4:
+      case 5:
+      case 6:
+        return 'Mid';
         break;
-      case 'c':
-        return 'cat';
+  
+      case 7:
+      case 8:
+      case 9:
+        return 'High';
         break;
-      default: 
-        return 'stuff';
-  }
+    };
   
   
     // Only change code above this line
     return answer;
   }
   
-  switchOfStuff(1);
+  sequentialSizes(1);
