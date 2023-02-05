e3aa3d50aff55faf312774e202a44f391dd5a045
Tim <timuravezov123@mail.ru>
2023-02-05 21:37:24 +0500

    127

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,9 +1,12 @@
-function removeFirstTwo(list) {
-  // Only change code below this line
-  const [a, b, ...shorterList] = list; // Change this line
-  // Only change code above this line
-  return shorterList;
-}
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
 
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const sourceWithoutFirstTwo = removeFirstTwo(source);
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0; 
+// Only change code above this line
