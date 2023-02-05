a76abb7646766e801da12f989c43f4e45d4a53bd
Tim <timuravezov123@mail.ru>
2023-02-05 20:23:25 +0500

    122

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,6 +1,9 @@
-const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
-let arr2;
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
 
-arr2 = [...arr1];  // Change this line
-
-console.log(arr2);
+// Only change code below this line
+const {today, tomorrow} = HIGH_TEMPERATURES;
+// Only change code above this line
