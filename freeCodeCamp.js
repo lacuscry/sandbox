c6ddc1ed20e7c489d14fda50d1fd4c5bf44b331d
Tim <timuravezov123@mail.ru>
2023-02-05 22:54:42 +0500

    142

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,10 +1,14 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-  // responseFromServer is set to true to represent a successful response from a server
-  let responseFromServer = true;
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
     
   if(responseFromServer) {
     resolve("We got the data");
   } else {  
     reject("Data not received");
   }
-}).then((result) => console.log(result));
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+}).catch((error) => console.log(error));
