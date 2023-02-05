c010b2dd52f8dba2cdfddccba59139e85d5cc49c
Tim <timuravezov123@mail.ru>
2023-02-05 20:21:39 +0500

    120

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,1 @@
-// Only change code below this line
-const increment = (number, value = 1) => number + value;
-// Only change code above this line
+const sum = (...args) => args.reduce((a, b) => a + b, 0);
