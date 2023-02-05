78ada0b34ca6edc86a5d49f92176953c23f87c57
Tim <timuravezov123@mail.ru>
2023-02-05 20:15:52 +0500

    115

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,9 +1,8 @@
-function checkScope() {
-  let i = 'function scope';
-  if (true) {
-    let i = 'block scope';
-    console.log('Block scope i is: ', i);
-  }
-  console.log('Function scope i is: ', i);
-  return i;
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s.sort()
+
+  // Only change code above this line
 }
+editInPlace();
