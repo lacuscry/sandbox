79303b1ca4d83ee3c7b6edf4642656b53651e7fe
Tim <timuravezov123@mail.ru>
2023-02-05 22:37:13 +0500

    133

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,20 +1,7 @@
-// Only change code below this line
-class Thermostat {
-  constructor(deg) {
-    this._temperature = 5 / 9 * (deg - 32);
-  }
-
-  get temperature() {
-    return this._temperature;
-  }
-
-  set temperature(value) {
-    this._temperature = value;
-  }
-}
-// Only change code above this line
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
+<html>
+  <body>
+    <!-- Only change code below this line -->
+      <script src="index.js" type="module"></script>
+    <!-- Only change code above this line -->
+  </body>
+</html>
