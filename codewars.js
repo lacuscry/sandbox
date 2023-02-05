de4da24f97af9347e707bcf361f39aa853bf68e0
Tim <timuravezov123@mail.ru>
2023-02-05 15:13:25 +0500

    43

--- codewars.js
+++ codewars.js
@@ -1,11 +1,1 @@
-function SeriesSum(n){
-    let result = n ? 1 : 0;
-    let count = 1;
-  
-    for (let i = 1; count < n; count++) {
-      i += 3;
-      result += 1 / i;
-    }
-  
-    return result % 1 == 0 ? `${result}.00` : `${result.toFixed(2)}`;
-}
+const greet = n => `Hello, ${n} how are you doing today?`;
