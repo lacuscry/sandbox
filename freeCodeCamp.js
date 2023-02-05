a45fdff796c296b4f1c9ece69d74dc1109f3d9e6
Tim <timuravezov123@mail.ru>
2023-02-05 21:51:51 +0500

    128

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,12 +1,13 @@
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
 };
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems =  [...arr].map(i => `<li class="text-warning">${i}</li>`);
+  // Only change code above this line
+  return failureItems;
+}
 
-// Only change code below this line
-const half = ({max, min}) => (max + min) / 2.0; 
-// Only change code above this line
+const failuresList = makeList(result.failure);
