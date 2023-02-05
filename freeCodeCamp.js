7e39f805939bb587f7dcd8c0c22ee3f004d200c5
Tim <timuravezov123@mail.ru>
2023-02-05 21:59:23 +0500

    129

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,13 +1,5 @@
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"]
-};
-function makeList(arr) {
+const createPerson = (name, age, gender) => {
   // Only change code below this line
-  const failureItems =  [...arr].map(i => `<li class="text-warning">${i}</li>`);
+  return {name, age, gender};
   // Only change code above this line
-  return failureItems;
-}
-
-const failuresList = makeList(result.failure);
+};
