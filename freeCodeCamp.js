634c59ce4c3cf33a4bc90ef80c1bc038c0726f63
Tim <timuravezov123@mail.ru>
2023-02-05 20:22:24 +0500

    121

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,1 +1,6 @@
-const sum = (...args) => args.reduce((a, b) => a + b, 0);
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
