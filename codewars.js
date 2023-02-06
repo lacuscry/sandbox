002e76ea414cf8ccf16616910ef2ea8ba9c60d53
Tim <timuravezov123@mail.ru>
2023-02-06 19:03:03 +0500

    56

--- codewars.js
+++ codewars.js
@@ -1,18 +1,1 @@
-function twoSum(numbers, target) {
-    let result;
-    
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] + numbers[i - 1] === target) {
-          result = [i, i - 1];
-          break;
-        } else {
-          for (let j = numbers.length; j >= 0; j--) {
-            if (numbers[i] + numbers[j] === target) {
-              result = [i, j];
-            }
-          }
-        }
-    }
-  
-    return result;
-}
+const switchItUp = num => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][num];
