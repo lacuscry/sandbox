8848e4353c9287304a867cdeda7734c591b4ed29
Tim <timuravezov123@mail.ru>
2023-02-06 15:10:02 +0500

    48

--- codewars.js
+++ codewars.js
@@ -1,1 +1,27 @@
-const setAlarm = (employed, vacation) => employed && !vacation;
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let result = dadYearsOld;
+    
+    
+    while (result > 0 && result / sonYearsOld !== 2) {
+      result -= 1;
+    }
+    
+    
+    if (result) {
+      return dadYearsOld - result;
+    } else {
+        result = dadYearsOld;
+  
+        for (let i = dadYearsOld; i < dadYearsOld + sonYearsOld; i++) {
+          result += 1;
+          
+          if (result / sonYearsOld === 2) {
+            result -= dadYearsOld;
+            
+            break;
+          }
+        }
+      
+      return result;
+    }
+  }
