1d519d2f227e2ea4bcbb6980c6085eb6fb677111
Tim <timuravezov123@mail.ru>
2023-02-07 02:06:40 +0500

    58

--- codewars.js
+++ codewars.js
@@ -1,3 +1,10 @@
-const letters = ['a', 'e', 'i', 'o', 'u'];
-
-const disemvowel = str => str.split(' ').map(w => [...w].filter(l => !letters.includes(l.toLowerCase()))).map(a => a.join('')).join(' ');
+function sortArray(array) {
+    const odd = [];
+    const even = [];
+    
+    const arr = array.map((a, i) => a % 2 === 0 ? a : odd.push(a) && even.push(i) && 0);
+    
+    even.forEach((a, i) => arr[even[i]] ? 0 : arr[even[i]] = odd.sort((a, b) => a - b)[i]);
+    
+    return arr;
+  }
