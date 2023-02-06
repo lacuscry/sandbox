3a5447cff3bcd754fb037b4584d764cd1df76f23
Tim <timuravezov123@mail.ru>
2023-02-06 18:52:25 +0500

    55

--- codewars.js
+++ codewars.js
@@ -1,1 +1,18 @@
-const solution = string => string.split('').map(l => l == l.toLowerCase() ? l : ` ${l}`).join('');
+function twoSum(numbers, target) {
+    let result;
+    
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] + numbers[i - 1] === target) {
+          result = [i, i - 1];
+          break;
+        } else {
+          for (let j = numbers.length; j >= 0; j--) {
+            if (numbers[i] + numbers[j] === target) {
+              result = [i, j];
+            }
+          }
+        }
+    }
+  
+    return result;
+}
