01f10c4653ff0cdf8f1d4f65eabe375c0be785d1
Tim <timuravezov123@mail.ru>
2023-02-06 13:52:50 +0500

    46

--- codewars.js
+++ codewars.js
@@ -1,7 +1,19 @@
-function duplicateCount(text){
-    let result = [];
-
-    text && [...text.toLowerCase()].map((cur, i, arr) => arr.indexOf(cur) !== i && result.push(cur));
-  
-    return [...new Set(result)].length || 0;
-}
+function bouncingBall(h,  bounce,  window) {
+    let count = 1;
+    
+    if (bounce < 1) {
+      let height = h;
+      
+      for (let i = 0; height > window; i++) {
+        height *= bounce;
+        
+        if (height > window) {
+          count += 2;
+        } else {
+          break;
+        }
+      }
+    }
+    
+    return 0 < bounce && bounce < 1 && window < h ? count : -1;
+  }
