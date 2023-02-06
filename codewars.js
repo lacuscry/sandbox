a43988376c3451c4c48af6e4382a7be6a00e4161
Tim <timuravezov123@mail.ru>
2023-02-06 23:00:01 +0500

    57

--- codewars.js
+++ codewars.js
@@ -1,1 +1,3 @@
-const switchItUp = num => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][num];
+const letters = ['a', 'e', 'i', 'o', 'u'];
+
+const disemvowel = str => str.split(' ').map(w => [...w].filter(l => !letters.includes(l.toLowerCase()))).map(a => a.join('')).join(' ');
