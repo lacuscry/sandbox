5aa1257a0b189d5a621fd1560212da0e77b6e123
Tim <timuravezov123@mail.ru>
2023-02-09 23:37:14 +0500

    174

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,4 @@
-let repeatNum = "42 42 42";
-let reRegex = /^(\d+) \1\ \1$/g; // Change this line
-let result = repeatNum.match(reRegex);
+let str = "one two three";
+let fixRegex = /(one) (two) (three)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
