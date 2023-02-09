6215d3736e8df26f1d74226c5585cdb09c3df129
Tim <timuravezov123@mail.ru>
2023-02-10 01:35:10 +0500

    206

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,25 +1,26 @@
-const users = {
+let users = {
     Alan: {
+      age: 27,
       online: false
     },
     Jeff: {
+      age: 32,
       online: true
     },
     Sarah: {
+      age: 48,
       online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
     }
-  }
+  };
   
-  function countOnline(usersObj) {
+  function getArrayOfUsers(obj) {
     // Only change code below this line
-    let countOnline = 0
-  
-    for (let user in usersObj) {
-        countOnline += +usersObj[user].online;
-    }
-  
-    return countOnline;
+    return Object.keys(obj);
     // Only change code above this line
   }
   
-  console.log(countOnline(users));
+  console.log(getArrayOfUsers(users));
