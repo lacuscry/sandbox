994e23c24a922c168702bf4e962a0793d9c30c6b
Tim <timuravezov123@mail.ru>
2023-02-10 03:35:01 +0500

    220

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,11 +1,3 @@
-function frankenSplice(arr1, arr2, n) {
-    const newArr = [...arr1];
-  
-    newArr.unshift(arr2.slice(0, n));
-    
-    newArr.push(arr2.slice(n, arr2.length));
-    
-    return newArr.flat();
-  }
-  
-  frankenSplice([1, 2, 3], [4, 5, 6], 1);
+const bouncer = arr => arr.filter(a => a);
+
+bouncer([7, "ate", "", false, 9]);
