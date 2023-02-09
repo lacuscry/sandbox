4bfd35cc65cffa8b59ed6bb8231f72a27ac46584
Tim <timuravezov123@mail.ru>
2023-02-10 01:02:49 +0500

    200

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,14 +1,13 @@
-let myNestedArray = [
-    // Only change code below this line
-    [
-      ['deep'],
-      [
-        ['deeper'],
-        [
-          [true],
-          ['deepest']
-        ]
-      ]
-    ]
-    // Only change code above this line
-  ];
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  foods.bananas = 13;
+  foods.grapes = 35;
+  foods.strawberries = 27;
+  // Only change code above this line
+  
+  console.log(foods);
