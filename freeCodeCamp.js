7398b768d4b29a8526ddc451d07f1904fe813fac
Tim <timuravezov123@mail.ru>
2023-02-09 23:48:30 +0500

    175

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,4 +1,3 @@
-let str = "one two three";
-let fixRegex = /(one) (two) (three)/; // Change this line
-let replaceText = "$3 $2 $1"; // Change this line
-let result = str.replace(fixRegex, replaceText);
+let hello = "   Hello, World!  ";
+let wsRegex = /\s{3}Hello, World!\s{2}/; // Change this line
+let result = hello.replace(wsRegex, "Hello, World!"); // Change this line
