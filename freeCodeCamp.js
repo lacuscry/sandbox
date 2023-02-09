348d98c300adbe457dae3954acf2c23ecd8dc6fd
Tim <timuravezov123@mail.ru>
2023-02-09 23:49:02 +0500

    176

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,7 @@
-let hello = "   Hello, World!  ";
-let wsRegex = /\s{3}Hello, World!\s{2}/; // Change this line
-let result = hello.replace(wsRegex, "Hello, World!"); // Change this line
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+let sumAB = a + b;
+console.log(a);
