ee56edf20c689dbde743513f26be9837ff6c4900
Tim <timuravezov123@mail.ru>
2023-02-09 13:49:43 +0500

    74

--- codewars.js
+++ codewars.js
@@ -1,3 +1,20 @@
-class SmallestIntegerFinder {
-    findSmallestInt = args => args.sort((a, b) => a - b)[0];
+function towerBuilder(nFloors) {
+    const tree = [];
+    let star = 1;
+  
+      
+    for (let i = 0; i < nFloors; i++) {
+      let str = '*'.repeat(star);
+      
+      if (nFloors - 1 === i) {
+          tree.push(str)
+      } else {
+        tree.push(' '.repeat(nFloors - i - 1) + str + ' '.repeat(nFloors - i - 1))
+      }
+      
+      star += 2;
+    }
+    
+  
+    return tree;
   }
