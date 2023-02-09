07f07b5950f09d029e03759762fc378c0056b03d
Tim <timuravezov123@mail.ru>
2023-02-09 21:09:46 +0500

    170

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,3 @@
-let timStr = "Timmmmber";
-let timRegex = /Tim{4}ber/; // Change this line
-let result = timRegex.test(timStr);
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
