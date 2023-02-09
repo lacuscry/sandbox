8dbf1af83575371dc6b6aed5eb5059c4860c1a35
Tim <timuravezov123@mail.ru>
2023-02-10 03:34:26 +0500

    219

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,11 @@
-const titleCase = str => str.toLowerCase().replace(/(\s\w)|(^.)/g, x => x.toUpperCase());
-
-titleCase("I'm a little tea pot");
+function frankenSplice(arr1, arr2, n) {
+    const newArr = [...arr1];
+  
+    newArr.unshift(arr2.slice(0, n));
+    
+    newArr.push(arr2.slice(n, arr2.length));
+    
+    return newArr.flat();
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
