17e4702dbe894139017dda1242fb167c565a959b
Tim <timuravezov123@mail.ru>
2023-02-10 19:11:11 +0500

    81

--- codewars.js
+++ codewars.js
@@ -1,1 +1,10 @@
-const greet = () => 'hello world!';
+function findNextSquare(sq) {
+  let nextSquare = sq;
+  
+  do {
+      nextSquare += 1;
+  } while (Math.sqrt(nextSquare) % 1 !== 0)
+  
+
+  return Math.sqrt(sq) % 1 === 0 ? nextSquare : -1;
+}
