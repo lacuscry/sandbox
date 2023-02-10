7e49e7123525f9b741564e213c8ea2475d615aeb
Tim <timuravezov123@mail.ru>
2023-02-10 17:06:10 +0500

    76

--- codewars.js
+++ codewars.js
@@ -1,24 +1,11 @@
-function findEvenIndex(arr){
-  let center;
+function order(words){
+  const string = [];
+    
+  const nums = words.match(/[0-9]/g);
   
-  arr.forEach((el, i) => {
-      let leftSum = 0;
-      let rightSum = 0;
-      
-    
-      for (let k = i; k < arr.length; k++) {
-          leftSum += arr[k]
-      }
-    
-      for (let j = i; j >= 0; j--) {
-          rightSum += arr[j]
-      }
-    
-    
-      if (leftSum === rightSum) {
-          center = i;
-      }
-  });
+  nums?.forEach((a, i) => {
+    string[nums[i]] = words.split(' ')[i];
+  })
   
-  return arr.every(el => el === 0) ? 0 : center !== undefined ? center : -1;
+  return string.filter(a => a).join(' ');
 }
