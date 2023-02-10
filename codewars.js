bb8bf499b0a03f59c0ff564fe07461c51beb22a6
Tim <timuravezov123@mail.ru>
2023-02-10 19:00:59 +0500

    79

--- codewars.js
+++ codewars.js
@@ -1,1 +1,22 @@
-const DNAtoRNA = dna => dna.replace(/T/g,'U');
+const rps = (p1, p2) => {
+  let total = 0;
+
+  switch(p1 + p2) {
+      case 'scissorspaper':
+      case 'paperrock':
+      case 'rockscissors':
+        total++;
+        break;
+      
+      case 'paperscissors':
+      case 'rockpaper':
+      case 'scissorsrock':
+        total--;
+        break;
+      
+      default:
+        break;
+  }
+
+  return total > 0 ? 'Player 1 won!' : total < 0 ? 'Player 2 won!' : 'Draw!';
+};
