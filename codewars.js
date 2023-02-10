0050d0d6ebe2f2b8f84ed45e5d89668698240003
Tim <timuravezov123@mail.ru>
2023-02-10 21:53:56 +0500

    88

--- codewars.js
+++ codewars.js
@@ -1,1 +1,7 @@
-const solution = (str, ending) => str.endsWith(ending);
+function duplicateEncode(word){
+  const repeated = {};
+        
+  word.toLowerCase().split('').forEach((a, i, arr) => i === arr.indexOf(a) ? repeated[a] = '(' : repeated[a] = ')');
+  
+  return word.toLowerCase().replace(/./g, x => repeated[x]);
+}
