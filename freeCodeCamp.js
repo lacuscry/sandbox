e40cd4e954f90a5e12d978110ec61183935adf70
Tim <timuravezov123@mail.ru>
2023-02-11 15:14:57 +0500

    221

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,7 @@
-const bouncer = arr => arr.filter(a => a);
-
-bouncer([7, "ate", "", false, 9]);
+function getIndexToIns(arr, num) {
+    arr.push(num);
+  
+    return arr.sort((a, b) => a - b).indexOf(num);
+  }
+  
+  getIndexToIns([40, 60], 50);
