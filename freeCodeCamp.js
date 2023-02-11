e568adff45036abac9efa256f2c8c7e9ca95c4b5
Tim <timuravezov123@mail.ru>
2023-02-11 17:39:16 +0500

    247

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,17 +1,4 @@
-let bird = {
-    name: "Donald",
-    numLegs: 2
-  };
-  
-  let boat = {
-    name: "Warrior",
-    type: "race-boat"
-  };
-  
-  // Only change code below this line
-  const glideMixin = function(obj) {
-      obj.glide = () => console.log('Flying, wooosh!');
+function Bird() {
+    let weight = 15;
+    this.getWeight = () => weight;
   }
-  
-  glideMixin(bird);
-  glideMixin(boat);
