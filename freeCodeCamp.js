6434e4448a4c37fc601e9d8810199ed643fb3ddb
Tim <timuravezov123@mail.ru>
2023-02-11 16:18:16 +0500

    243

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,15 +1,13 @@
 function Animal() { }
-
-Animal.prototype = {
-  constructor: Animal,
-  eat: function() {
-    console.log("nom nom nom");
-  }
-};
-
+function Bird() { }
 function Dog() { }
 
-// Only change code below this line
+Bird.prototype = Object.create(Animal.prototype);
 Dog.prototype = Object.create(Animal.prototype);
 
+// Only change code below this line
+let duck = new Bird();
 let beagle = new Dog();
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
