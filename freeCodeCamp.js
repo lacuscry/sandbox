02c2384f4550c06923f3c38b36156ce798f1edd0
Tim <timuravezov123@mail.ru>
2023-02-11 17:47:38 +0500

    250

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,14 +1,21 @@
-const funModule = (() => {
-    return {
-        isCuteMixin: function(obj) {
-            obj.isCute = function() {
-              return true;
-            };
-        },
-        singMixin: function(obj) {
-            obj.sing = function() {
-              console.log("Singing to an awesome tune");
-            };
-        }
-    }
-})();
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
