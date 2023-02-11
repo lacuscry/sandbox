af63cdcee49b8a7d8634b9473ac67fb599ac8dcc
Tim <timuravezov123@mail.ru>
2023-02-11 21:44:31 +0500

    270

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,14 +1,7 @@
-// Only change code below this line
-function urlSlug(title) {
-    let str = title
-      .toLowerCase()
-      .split(' ')
-      .filter(a => a)
-      .map((a, i, arr) => i < arr.length - 1 ? `${a}-` : a)
-      .join('');
-
-    return str;
-}
-// Only change code above this line
-urlSlug(" Winter Is  Coming")
-urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.every(a => a > 0);
+    // Only change code above this line
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
