e28bd92e09eb6e7e161eaae15cc470bc9eb08e9d
Tim <timuravezov123@mail.ru>
2023-02-11 21:13:49 +0500

    265

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,8 +1,7 @@
-const squareList = arr => {
+function alphabeticalOrder(arr) {
     // Only change code below this line
-    return arr.filter(a => !(a % 1) && a > 1).map(a => a * a);
+    return arr.sort();
     // Only change code above this line
-  };
+  }
   
-  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
-  console.log(squaredIntegers);
+  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
