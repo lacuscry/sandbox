d7fb6a25dff2831a7341f6a09befbadf45e52952
Tim <timuravezov123@mail.ru>
2023-02-12 02:04:35 +0500

    279

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,24 +1,6 @@
-function translatePigLatin(str) {
-    let result = str;
-  
-    for (let i = 0; ;i++) {
-      if (/[aeiou]/.test(str[i])) {
-        break;
-      } else {
-        result = result.slice(1, str.length) + str[i];
-      }
-    }
-    
-    if (result !== str || !/[aeiou]/g.test(result)) {
-        result += 'ay';
-    } else {
-        result += 'way';
-    }
-  
-    return result;
+function myReplace(str, before, after) {
+    return str.replace(before, a => after.replace(/./, b => a[0] === a[0].toUpperCase() ? b.toUpperCase() : b.toLowerCase()));
   }
   
-  translatePigLatin("rhythm")
-  translatePigLatin("algorithm")
-  translatePigLatin("glove")
-  translatePigLatin("consonant");
+  myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
+  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
