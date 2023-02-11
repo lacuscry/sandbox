d6d3c6463fb2335ac7dfb1bfd739a164296a05bf
Tim <timuravezov123@mail.ru>
2023-02-11 15:58:47 +0500

    237

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -2,9 +2,14 @@ function Dog(name) {
     this.name = name;
   }
   
+  // Only change code below this line
   Dog.prototype = {
-    // Only change code below this line
+    constructor : Dog,
     numLegs: 4,
-    eat: () => {},
-    describe: () => {}
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
   };
