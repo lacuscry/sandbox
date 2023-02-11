e870c85cea328af57578fad84a8963b675a7438d
Tim <timuravezov123@mail.ru>
2023-02-11 18:11:33 +0500

    255

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,8 +1,26 @@
 // The global variable
-let fixedValue = 4;
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
-// Only change code below this line
-function incrementer(fixedValue) {
-  return fixedValue + 1;
-  // Only change code above this line
+// Change code below this line
+function add(bookList, bookName) {
+  const newArr = [...bookList];
+
+  newArr.push(bookName);
+  
+  return newArr;
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookList, bookName) {
+  const newArr = [...bookList];
+
+  const book_index = newArr.indexOf(bookName);
+  if (book_index >= 0) {
+
+    newArr.splice(book_index, 1);
+    return newArr;
+
+    // Change code above this line
+    }
 }
