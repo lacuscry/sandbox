9b41b3f3f8b942aa36904bf096e897d5d024d1a8
Tim <timuravezov123@mail.ru>
2023-02-11 21:15:07 +0500

    266

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,7 +1,9 @@
-function alphabeticalOrder(arr) {
-    // Only change code below this line
-    return arr.sort();
-    // Only change code above this line
-  }
-  
-  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return [...arr].sort((a, b) => a - b);
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
