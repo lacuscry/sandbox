2d6a394e7b97cc0ace76ea34127140549771d062
Tim <timuravezov123@mail.ru>
2023-02-11 23:59:06 +0500

    276

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,5 +1,6 @@
-function destroyer(array, ...other) {
-    return array.filter(a => other.indexOf(a) < 0);
+function whatIsInAName(collection, source) {
+    return collection.filter(a => Object.keys(source).every(b => source[b] === a[b]));
   }
   
-  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet", });
+  whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 3, "bat": 1 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
