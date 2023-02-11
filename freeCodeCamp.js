fb3eadaf6e405ea2388afd579136264d701678c6
Tim <timuravezov123@mail.ru>
2023-02-11 15:29:49 +0500

    224

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,13 +1,4 @@
-function chunkArrayInGroups(arr, size) {
-    const res = [];
-  
-    while (0 < arr.length) {
-        res.push(arr.splice(0, size))
-    }
-  
-    return res;
-  }
-  
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)
-  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+let dog = {
+    name: 'Dog',
+    numLegs: 4
+};
