a2bd82fb5e92a6cb21a83b5c899473b05cc4879a
Tim <timuravezov123@mail.ru>
2023-02-11 15:49:52 +0500

    234

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -2,8 +2,18 @@ function Dog(name) {
     this.name = name;
   }
   
-  
-  
-  // Only change code above this line
-  let beagle = new Dog("Snoopy");
   Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+  // Only change code below this line
+  for (let property in beagle) {
+      if (beagle.hasOwnProperty(property)) {
+          ownProps.push(property);
+      } else {
+          prototypeProps.push(property);
+      }
+  }s
