a329bbcb906a2d056b5493717d84d5aa812e3d8c
Tim <timuravezov123@mail.ru>
2023-02-12 03:11:42 +0500

    281

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,10 +1,17 @@
-function pairElement(str) {
-    return str.split('').map(a => ({
-      A: ['A', 'T'],
-      T: ['T', 'A'],
-      C: ['C', 'G'],
-      G: ['G', 'C']
-    })[a]);
+function fearNotLetter(str) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+    let missing;
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i - 1] !== alphabet[alphabet.indexOf(str[i]) - 1]) {
+        missing = alphabet[alphabet.indexOf(str[i]) - 1];
+      }
+    }
+  
+    return missing;
   }
   
-  pairElement("GCG");
+  fearNotLetter("stvwx")
+  fearNotLetter("abcdefghjklmno")
+  fearNotLetter("abce")
