aeca83d3f219662d9994a88b101f6bde21cf27d5
Tim <timuravezov123@mail.ru>
2023-02-11 17:29:51 +0500

    246

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,14 +1,17 @@
-function Bird() { }
-
-Bird.prototype.fly = function() { return "I am flying!"; };
-
-function Penguin() { }
-Penguin.prototype = Object.create(Bird.prototype);
-Penguin.prototype.constructor = Penguin;
-
-// Only change code below this line
-Penguin.prototype.fly = () => "Alas, this is a flightless bird.";
-// Only change code above this line
-
-let penguin = new Penguin();
-console.log(penguin.fly());
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  // Only change code below this line
+  const glideMixin = function(obj) {
+      obj.glide = () => console.log('Flying, wooosh!');
+  }
+  
+  glideMixin(bird);
+  glideMixin(boat);
