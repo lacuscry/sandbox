abe87cc16a42215747de32bc21cf9366d0b108e2
Tim <timuravezov123@mail.ru>
2023-02-11 15:38:30 +0500

    230

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,8 +1,7 @@
-function Dog() {
-    this.name = "Rupert";
-    this.color = "brown";
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
     this.numLegs = 4;
-  }
-  
-  // Only change code below this line
-  const hound = new Dog();
+}
+
+const terrier = new Dog('Dog', 'black');
