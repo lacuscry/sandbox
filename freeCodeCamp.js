a6960c5d1ce15472ddf8d6a62e8e6837b68e113e
Tim <timuravezov123@mail.ru>
2023-02-12 18:50:36 +0500

    284

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,14 +1,25 @@
-function convertHTML(str) {
-    const specialChars = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&apos;',
-      '<>': '&lt;'
-    } 
-  
-    return str.replace(/./g, a => specialChars.hasOwnProperty(a) ? specialChars[a] : a);
+function sumFibs(num) {
+  const arr = [1];
+  let sum = 0;
+
+  for (let i = 1; i < num; i++) {
+    if (arr.length > 3) {
+      arr.push(arr.slice(-2).reduce((a, b) => a + b));
+    } else {
+      arr.push(i);
+    }
   }
-  
-  convertHTML("Dolce & Gabbana");
+
+  arr.reverse().forEach(a => {
+    if (a % 2 && a <= num) {
+      sum += a;
+    }
+  })
+
+  return sum;
+}
+
+sumFibs(3); //2
+sumFibs(4); //5
+sumFibs(1000); //1785
+sumFibs(75025); //135721
