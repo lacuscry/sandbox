260cc5f393b3c31c4a6aa755d9c21c75133395dd
Tim <timuravezov123@mail.ru>
2023-02-14 00:06:49 +0500

    92

--- codewars.js
+++ codewars.js
@@ -1,1 +1,17 @@
-const wave = str => Array(str.length).fill(str).map((w, i) => w.replace(/[a-z]/gi, (l, j) => i === j ? l.toUpperCase() : l)).filter(a => /[A-Z]/g.test(a));
+function longestConsec(strarr, k) {
+    if (!strarr.length || k < 1 || k > strarr.length) {
+       return "";
+    }
+   
+    const arr = [];
+   
+    for (let i = 0; i < strarr.length; i++) {
+         arr[i] = strarr.slice(i, i + k);
+    }
+   
+    const words = arr.map(a => a.reduce((acc, b) => acc + b));
+ 
+    const length = words.map(a => a.length);
+   
+    return words[length.indexOf(Math.max.apply(null, length))];
+ }
