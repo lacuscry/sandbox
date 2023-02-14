3494b7e5185c27739e02a2251f5f41e50e54b1eb
Tim <timuravezov123@mail.ru>
2023-02-14 14:48:55 +0500

    93

--- codewars.js
+++ codewars.js
@@ -1,17 +1,1 @@
-function longestConsec(strarr, k) {
-    if (!strarr.length || k < 1 || k > strarr.length) {
-       return "";
-    }
-   
-    const arr = [];
-   
-    for (let i = 0; i < strarr.length; i++) {
-         arr[i] = strarr.slice(i, i + k);
-    }
-   
-    const words = arr.map(a => a.reduce((acc, b) => acc + b));
- 
-    const length = words.map(a => a.length);
-   
-    return words[length.indexOf(Math.max.apply(null, length))];
- }
+const domainName = url => url.match(/(?![/.])+(?!w)+[a-z0-9]?(-*[a-z0-9])+[/.]/g, '')[0].replace(/[./:]/g, '');
