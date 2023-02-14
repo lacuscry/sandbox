1e50d019120aad6382b9856d15989883c54aa47c
Tim <timuravezov123@mail.ru>
2023-02-14 20:46:09 +0500

    106

--- codewars.js
+++ codewars.js
@@ -1,11 +1,1 @@
-function solution(number){
-    let sum = 0;
-    
-    for (let i = 0; i < number; i++) {
-        if (!(i % 5) || !(i % 3)) {
-            sum += i;
-        }
-    }
-    
-    return sum;
-  }
+const spinWords = string => string.split(' ').map(a => a.length > 4 ? a.split('').reverse().join('') : a).join(' ');
