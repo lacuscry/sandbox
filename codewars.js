a673735bc6ce1e8e43fdf2c39c6ed71bdfbc989e
Tim <timuravezov123@mail.ru>
2023-02-14 22:12:29 +0500

    111

--- codewars.js
+++ codewars.js
@@ -1,1 +1,11 @@
-const findOutlier = integers => integers.filter(a => a % 2).length > 1 ? +integers.filter(a => !(a % 2)) : +integers.filter(a => a % 2);
+function dontGiveMeFive(start, end) {
+    const nums = []  
+  
+    for (let i = start; i <= end; i++) {
+      if ((i % 5 || !(i % 10)) && (i < 50 || i > 59)) {
+        nums.push(i);
+      }
+    }
+  
+    return nums.length;
+};
