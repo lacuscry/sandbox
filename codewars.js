fb4e0cc4ff97d531c0cd11df842bc05c2a303d88
Tim <timuravezov123@mail.ru>
2023-02-14 15:15:48 +0500

    95

--- codewars.js
+++ codewars.js
@@ -1,16 +1,1 @@
-function comp(array1, array2){
-   if (!array1 || !array2) {
-       return false
-   }
-   
-   const isSame = array2.every(a => {
-     if (array1.includes(Math.sqrt(a))) {
-         array1[array1.indexOf(Math.sqrt(a))] = 0;
-         return true;
-     } else {
-         return false;
-     }
-   });
-   
-   return isSame;
- }
+const getCount = str => str.replace(/[^aeiou]/g, '').length;
