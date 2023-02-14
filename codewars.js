6be7d330d4f3025cddf87fd8ceaee59a35024e47
Tim <timuravezov123@mail.ru>
2023-02-14 15:08:08 +0500

    94

--- codewars.js
+++ codewars.js
@@ -1,1 +1,16 @@
-const domainName = url => url.match(/(?![/.])+(?!w)+[a-z0-9]?(-*[a-z0-9])+[/.]/g, '')[0].replace(/[./:]/g, '');
+function comp(array1, array2){
+   if (!array1 || !array2) {
+       return false
+   }
+   
+   const isSame = array2.every(a => {
+     if (array1.includes(Math.sqrt(a))) {
+         array1[array1.indexOf(Math.sqrt(a))] = 0;
+         return true;
+     } else {
+         return false;
+     }
+   });
+   
+   return isSame;
+ }
