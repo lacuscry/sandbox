8f7f6c66c7543ca5797666e533b05c04515581ef
Tim <timuravezov123@mail.ru>
2023-02-14 16:07:36 +0500

    96

--- codewars.js
+++ codewars.js
@@ -1,1 +1,18 @@
-const getCount = str => str.replace(/[^aeiou]/g, '').length;
+function findOdd(A) {
+   const repeatedNums = {}
+   
+   A.forEach(a => repeatedNums[a] = 0);
+   
+   A.forEach(a => {
+     if (repeatedNums.hasOwnProperty(a)) {
+       repeatedNums[a] += 1;
+     }
+   })
+   
+   
+   for (const key of Object.keys(repeatedNums)) {
+     if (repeatedNums[key] % 2) {
+       return +key;
+     }
+   }
+ }
