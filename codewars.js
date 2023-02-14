c99d9cf9182a6336a359b7d005d1d05ae5a37a4d
Tim <timuravezov123@mail.ru>
2023-02-15 00:29:46 +0500

    115

--- codewars.js
+++ codewars.js
@@ -1,1 +1,9 @@
-const countSheeps = arrayOfSheep => arrayOfSheep.filter(a => a).length;
+function narcissistic(value) {
+  for (let i = 0; i < 10; i++) {
+    if (value === value.toString().split('').map(a => a ** i).reduce((a, b) => a + b)) {
+      return true;
+    }
+  }
+  
+  return false;
+}
