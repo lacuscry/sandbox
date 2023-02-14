f12b4e0262489d42943d933879dc554dd887caa7
Tim <timuravezov123@mail.ru>
2023-02-14 20:40:36 +0500

    105

--- codewars.js
+++ codewars.js
@@ -1,1 +1,11 @@
-const noSpace = x => x.replace(/ /g, '');
+function solution(number){
+    let sum = 0;
+    
+    for (let i = 0; i < number; i++) {
+        if (!(i % 5) || !(i % 3)) {
+            sum += i;
+        }
+    }
+    
+    return sum;
+  }
