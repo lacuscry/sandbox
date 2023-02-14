e6875d67869ec8d41af4d49a17e62ad527c41609
Tim <timuravezov123@mail.ru>
2023-02-14 18:34:00 +0500

    101

--- codewars.js
+++ codewars.js
@@ -1,15 +1,1 @@
-function isValidWalk(walk) {
-   const n = walk.join('').match(/n/g)?.length;
-   const s = walk.join('').match(/s/g)?.length;
-   const w = walk.join('').match(/w/g)?.length;
-   const e = walk.join('').match(/e/g)?.length;
-   
-   
-   const vertical = Math.max(n, s) - Math.min(n, s);
-   const horizontal = Math.max(w, e) - Math.min(w, e);
-   
-   
-   const res = [vertical, horizontal].filter(a => Number.isInteger(a));
-   
-   return walk.length === 10 && res.length && res.reduce((a, b) => a + b) === 0 ? true : false
- }
+const boolToWord = bool => bool ? 'Yes' : 'No';
