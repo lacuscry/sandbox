186265d2de0c57ab221740a54916cd1db52b31d6
Tim <timuravezov123@mail.ru>
2023-02-15 19:22:35 +0500

    296

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,33 +1,15 @@
-const romanNums = {
-  1000: 'M', 900: 'CM', 500: 'D',
-  400: 'CD', 100: 'C', 90: 'XC',
-  50: 'L', 40: 'XL', 10: 'X',
-  9: 'IX', 5: 'V', 4: 'IV',
-  1: 'I'
+const caesarsAlphabet = {
+  A: 'N', B: 'O', C: 'P',
+  D: 'Q', E: 'R', F: 'S',
+  G: 'T', H: 'U', I: 'V',
+  J: 'W', K: 'X', L: 'Y',
+  M: 'Z', N: 'A', O: 'B',
+  P: 'C', Q: 'D', R: 'E',
+  S: 'F', T: 'G', U: 'H',
+  V: 'I', W: 'J', X: 'K',
+  Y: 'L', Z: 'M'
 }
 
-function convertToRoman(num) {
-  let total = 0;
-  let romanTotal = '';
+const rot13 = str => str.replace(/[A-Z]/g, a => caesarsAlphabet[a]);
 
-
-  for (let romanNum of Object.entries(romanNums).reverse()) {
-      if (num >= +romanNum[0]) {
-        while (total + +romanNum[0] <= num) {
-          total += +romanNum[0];
-          romanTotal += romanNum[1];
-        }
-      }
-  }
-
-    
- return romanTotal;
-}
-
-convertToRoman(36);
-convertToRoman(44);
-convertToRoman(45);
-convertToRoman(97);
-convertToRoman(400);
-convertToRoman(2014);
-convertToRoman(3999);
+rot13("SERR PBQR PNZC");
