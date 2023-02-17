39114daf33452bee834bf9fedfdc526daff4f7b5
Tim <timuravezov123@mail.ru>
2023-02-18 00:47:20 +0500

    124

--- codewars.js
+++ codewars.js
@@ -1,1 +1,9 @@
-const printerError = s => `${s.match(/[n-z]/g)?.join('').length || 0}/${s.length}`;
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    
+    for (;p0 < p; years++) {
+      p0 += Math.floor(p0 * percent / 100 + aug);
+    }
+    
+    return years;
+  }
