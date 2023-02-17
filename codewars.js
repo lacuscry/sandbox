18f9977e0fbd5259723299d92d2abfacf13c4de1
Tim <timuravezov123@mail.ru>
2023-02-18 00:09:46 +0500

    123

--- codewars.js
+++ codewars.js
@@ -1,7 +1,1 @@
-function digPow(n, p){
-    const nums = ('' + n).split('').map((a, i) => Math.pow(a, p + i));
-  
-    let res = nums.reduce((a, b) => a + b);
-   
-    return res === n * p && res / n % 1 ? 1 : res !== n * res / n || res / n % 1 ? -1 : res / n;
-  }
+const printerError = s => `${s.match(/[n-z]/g)?.join('').length || 0}/${s.length}`;
