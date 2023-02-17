a6f4fa743f0ace7a1a91d95d8aab5a72c0b9a45e
Tim <timuravezov123@mail.ru>
2023-02-18 00:51:43 +0500

    125

--- codewars.js
+++ codewars.js
@@ -1,9 +1,1 @@
-function nbYear(p0, percent, aug, p) {
-    let years = 0;
-    
-    for (;p0 < p; years++) {
-      p0 += Math.floor(p0 * percent / 100 + aug);
-    }
-    
-    return years;
-  }
+const century = year => Math.ceil(year / 100);
