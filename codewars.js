a27632a05784cceee2133756a1d6b07417e0249f
Tim <timuravezov123@mail.ru>
2023-02-19 02:48:24 +0500

    136

--- codewars.js
+++ codewars.js
@@ -1,12 +1,5 @@
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 
-'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z'];
-
-function findMissingLetter(array) {
-  const wordCase = array.every(a => alphabet.indexOf(a) >= 0);
+function countPositivesSumNegatives(input) {
+  const arr = input?.sort((a, b) => a - b).filter(a => a >= 0);
   
-  const res = array.map(a => alphabet.indexOf(a.toUpperCase())).map((a, i, arr) => a + 1 !== arr[i + 1] && alphabet[arr[i] + 1]).filter(a => a)[0];
-
-  return wordCase ? res : res.toLowerCase();
+  return input?.length ? [arr.filter(Number).length, input.slice(0, input.length - arr.length).reduce((a, b) => a + b, 0)] : [];
 }
