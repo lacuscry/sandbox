ce28f691da9ee575c1b9dda52b6c2b16352f59a0
Tim <timuravezov123@mail.ru>
2023-02-19 00:16:35 +0500

    135

--- codewars.js
+++ codewars.js
@@ -1,1 +1,12 @@
-const oddOrEven = array => array.reduce((a, b) => a + b, 0) % 2 ? 'odd' : 'even';
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 
+'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
+'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z'];
+
+function findMissingLetter(array) {
+  const wordCase = array.every(a => alphabet.indexOf(a) >= 0);
+  
+  const res = array.map(a => alphabet.indexOf(a.toUpperCase())).map((a, i, arr) => a + 1 !== arr[i + 1] && alphabet[arr[i] + 1]).filter(a => a)[0];
+
+  return wordCase ? res : res.toLowerCase();
+}
