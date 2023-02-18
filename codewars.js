569792e2c8424b9ea047a24c4733cd128998e868
Tim <timuravezov123@mail.ru>
2023-02-18 21:54:59 +0500

    131

--- codewars.js
+++ codewars.js
@@ -1,1 +1,9 @@
-const digitize = n => `${n}`.split('').map(Number).reverse();
+function moveZeros(arr) {
+    const newArr = [...arr.filter(a => a !== 0)];
+    
+    while (arr.length > newArr.length) {
+      newArr.push(0);
+    }
+    
+    return newArr;
+  }
