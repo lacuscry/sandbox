f15be7b444064ccb588d5117d53eedb9770ce9a8
Tim <timuravezov123@mail.ru>
2023-02-19 16:53:31 +0500

    142

--- codewars.js
+++ codewars.js
@@ -1,1 +1,5 @@
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuelLeft;
+function divisors(integer) {
+    const nums = Array.from({length: integer}, (a, b) => b > 1 && !(integer / b % 1) && b).filter(Number);
+    
+    return nums.length ? nums : `${integer} is prime`;
+  };
