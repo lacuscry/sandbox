4e95cdaacb5bd2e98f7eddcc6efe5e5cd15262da
Tim <timuravezov123@mail.ru>
2023-02-21 17:43:42 +0500

    147

--- codewars.js
+++ codewars.js
@@ -1,1 +1,5 @@
-const reverseWords = str => str.split(' ').reverse().join(' ');
+function bmi(weight, height) {
+    let res = weight / Math.pow(height, 2);
+    
+    return res <= 18.5 ? 'Underweight' : res <= 25 ? 'Normal' : res <=30 ? 'Overweight' : 'Obese';
+  }
